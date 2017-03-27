725f787e55afb534cd5ca5e77041fc565fb56d1b
Zaeem Siddiq <zaeem.siddiq@gmail.com>
2017-03-27 11:49:25 +1100

    created develop branch

---
+++ app/src/main/java/monash/sprintree/activities/Splash.java
@@ -0,0 +1,15 @@
+package monash.sprintree.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import monash.sprintree.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
