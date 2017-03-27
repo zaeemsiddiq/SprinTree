b8ab537513ff7ab8a6f45b20233bf2bda3a44920
Kalyani Singh <kalyani@dyn-118-139-11-214.its.monash.edu.au>
2017-03-27 11:57:30 +1100

    testSourceTree

--- app/src/main/java/monash/sprintree/activities/Splash.java
+++ app/src/main/java/monash/sprintree/activities/Splash.java
@@ -11,6 +11,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //test develop
+        //test branch develop
+        // branch
     }
 }
